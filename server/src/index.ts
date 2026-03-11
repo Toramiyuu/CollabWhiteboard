9aeb462303a55d0b34f047ec874d44695091ef3d
Harvey Takeru Miles Davis <harvey@Harveys-MacBook-Pro-5.local>
2026-03-11 09:44:30 +0800

    fix: add CORS headers to Express routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,6 +13,13 @@ const io = new Server(httpServer, {
 
 const rm = new RoomManager();
 
+app.use((_req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (_req.method === 'OPTIONS') { res.sendStatus(204); return; }
+  next();
+});
 app.use(express.json());
 
 // Create a new room
